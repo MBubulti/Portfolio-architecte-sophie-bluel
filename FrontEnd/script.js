5871b2e1acace7b39923e74b68bf28462442e2aa
Maxime Buffet <167359993+MBubulti@users.noreply.github.com>
2024-12-03 17:37:02 +0100

    Modifications Retour Visio 01/12
    - CSS (Visuel Mode édition / global)
    - Ajout des hyperliens sur la page login
    - Suppression des filtres en mode édition
    - Vérification token Valide
    - Fonction unique  pour filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,103 +3,49 @@ const appelApiProjets = await fetch("http://localhost:5678/api/works/");
 const projets = await appelApiProjets.json();
 
 //Appel des différentes Catégories pour les filtres
-const appelApiCategories = await fetch("http://localhost:5678/api/categories/")
-const categories = await appelApiCategories.json()
+const appelApiCategories = await fetch("http://localhost:5678/api/categories/");
+const categories = await appelApiCategories.json();
 
-//Fonction pour générer l'affichage de tous les projets
-function projetsGallery(Filtre){
-    // Récupération de l'élément DOM qui accueille la figure
-    const divProjets = document.querySelector(".gallery");
-    divProjets.innerHTML = "";
-  for (let i = 0; i <Filtre.length; i++){
-    const travaux = Filtre[i];
-    // Création d'une figure dédiée aux projets
-    const figuresProjet = document.createElement("figure");
-    figuresProjet.dataset.id = travaux.id
-    //Création des éléments de la figure
-    const imgProjet = document.createElement("img");
-    imgProjet.src = travaux.imageUrl
-    const titreProjet = document.createElement("figcaption");
-    titreProjet.innerText = travaux.title 
-    divProjets.appendChild(figuresProjet);
-    figuresProjet.appendChild(imgProjet);
-    figuresProjet.appendChild(titreProjet);
-  }  
-}
-
-//Fonction pour générer les filtres de tous les projets
-function filtresCategories(){
-  for (let i = 0; i< categories.length; i++){
-    const categoriesFiltres = categories[i];
-    const filtres = document.createElement("div")
-    filtres.dataset.id = categoriesFiltres.id
-    const filtresTitre =  document.createElement("p")
-    filtresTitre.innerText = categoriesFiltres.name
-    filtresNav.appendChild(filtres);
-    filtres.appendChild(filtresTitre);
-  }
-}
-
-//Barre de navigation Filtres des projets
-const filtresProjet = document.querySelector("#portfolio h2");
-const filtresNav = document.createElement("nav")
-filtresProjet.appendChild(filtresNav);
+//Affichage page selon présence du token  - PENSER A DELETE DU STORAGE LE TOKEN  QUAND ON FERME/QUITTE OU LOGOUT
+//Récupération du token en localStorage
+const token = localStorage.getItem("token");
 
 //Ajout de l'élément Tous dans le tableau & tri des filtres
-categories.push({id: 0, name: 'Tous'})
-categories.sort(function(a,b){
+categories.push({id: 0, name: "Tous"});
+categories.sort(function (a, b) {
   return a.id - b.id;
 });
 
-//Lancement des fonctions
-projetsGallery(projets);
+//Fonction qui vérifie localement l'expiration du token
+function tokenExpiration(token){
+  if(!token){
+    return null;
+  }
+  //Découpe du token en 3 élément, et sélection du payload
+  try{
+    const tokenPayload = token.split(".")[1];
+    const payload = JSON.parse(atob(tokenPayload));
+    //Déclaration d'une const  qui est le temps actuel en secondes
+    const temps = Math.floor(Date.now()/1000);
+    //Vérification du payload par rapport au temps
+    if(payload.exp && payload.exp < temps){
+      return null;
+    }
+    return payload;
+  }
+  //En cas d'erreur dans la vérification du token retourne un null
+  catch(error){
+    return null;
+  }
+}
 
-filtresCategories(categories);
+const tokenVerifie = tokenExpiration(token);
 
-//Filtrer les projets - Tous
-const btnTriTous = document.querySelector("div[data-id='0']")
-btnTriTous.addEventListener("click", function(){
-  projetsGallery(projets);
-})
-
-//Filtrer les projets - Objets
-const btnTriObjets = document.querySelector("div[data-id='1']")
-btnTriObjets.addEventListener("click", function(){
-  const projetsFiltreObjets = projets.filter(
-    projet => projet.category.name === "Objets"
-  )
-  projetsGallery(projetsFiltreObjets);
-})
-
-//Filtrer les projets - Appartements
-const btnTriApparts = document.querySelector("div[data-id='2']")
-btnTriApparts.addEventListener("click", function(){
-  const projetsFiltreApparts = projets.filter(
-    projet => projet.category.name === "Appartements"
-  )
-  projetsGallery(projetsFiltreApparts);
-})
-
-//Filtrer les projets - Hotels & Restaurants
-const btnTriHotels = document.querySelector("div[data-id='3']")
-btnTriHotels.addEventListener("click", function(){
-  const projetsFiltreHotels = projets.filter(
-    projet => projet.category.name === "Hotels & restaurants"
-  )
-  projetsGallery(projetsFiltreHotels);
-})
-
-//Affichage page selon présence du token  - PENSER A DELETE DU STORAGE LE TOKEN  QUAND ON FERME/QUITTE OU LOGOUT
-const token = localStorage.getItem("token");
-console.log('token présent')
-console.log(token)
-console.log(token !== undefined && token !== null)
-
-//IF qui permet les modifications en vérifiant le statut non null & undefined du token
-if (token !== undefined && token !== null){
+//If qui permet avec la vérification du token l'affichage spécifique
+if (tokenVerifie !== null) {
   //Transformation du mot login en logout
   const logModif = document.querySelector(".log");
-  logModif.innerHTML = "logout"
+  logModif.innerHTML = "logout";
 
   //Création de la barre mode édition
   const barreEdition = document.querySelector("body");
@@ -108,12 +54,98 @@ if (token !== undefined && token !== null){
   let iconeEdition = document.createElement("i");
   iconeEdition.classList.add("fa-regular", "fa-pen-to-square", "white");
   texteEdition.classList.add("white");
-  texteEdition.innerText = "Mode édition"
+  texteEdition.innerText = "Mode édition";
   divBarreEdition.classList.add("mode-edition");
-  divBarreEdition = barreEdition.insertBefore(divBarreEdition, document.querySelector("header"));
+  divBarreEdition = barreEdition.insertBefore(
+    divBarreEdition,
+    document.querySelector("header")
+  );
   divBarreEdition.appendChild(iconeEdition);
   divBarreEdition.appendChild(texteEdition);
   
-  //Ajout du bouton modifier
-  
+  //Bouton modifier
+  const selectionModifier = document.querySelector("#portfolio");
+  const divTitre = document.createElement("div");
+  divTitre.classList.add("div-titre")
+  const titrePortfolio = document.querySelector("#portfolio h2");
+  selectionModifier.insertBefore(divTitre, document.querySelector(".gallery"));
+  divTitre.appendChild(titrePortfolio);
+  const btnModifier = document.createElement("a");
+  btnModifier.setAttribute("href", "#")
+  divTitre.appendChild(btnModifier);
+  let iconeModification = document.createElement("i");
+  iconeModification.classList.add("fa-regular", "fa-pen-to-square");
+  btnModifier.appendChild(iconeModification);
+  const txtModifier = document.createElement("p");
+  txtModifier.innerText = "modifier";
+  btnModifier.appendChild(txtModifier);
 }
+
+//Fonction pour générer l'affichage de tous les projets
+function projetsGallery(filtre) {
+  // Récupération de l'élément DOM qui accueille la figure
+  const divProjets = document.querySelector(".gallery");
+  divProjets.innerHTML = "";
+  for (let i = 0; i < filtre.length; i++) {
+    const travaux = filtre[i];
+    // Création d'une figure dédiée aux projets
+    const figuresProjet = document.createElement("figure");
+    figuresProjet.dataset.id = travaux.id;
+    //Création des éléments de la figure
+    const imgProjet = document.createElement("img");
+    imgProjet.src = travaux.imageUrl;
+    const titreProjet = document.createElement("figcaption");
+    titreProjet.innerText = travaux.title;
+    divProjets.appendChild(figuresProjet);
+    figuresProjet.appendChild(imgProjet);
+    figuresProjet.appendChild(titreProjet);
+  }
+}
+
+//Fonction pour générer les boutons filtres de tous les projets
+function filtresCategories() {
+  //Permet de ne pas générer les filtres si on a un token Vérifié
+  if (tokenVerifie !== null){
+    return;
+  }
+  const filtresNav = document.createElement("nav");
+  for (let i = 0; i < categories.length; i++) {
+      //Barre de navigation Filtres des projets
+      const filtresProjet = document.querySelector("#portfolio h2");
+      const categoriesFiltres = categories[i];
+      const filtres = document.createElement("div");
+      filtres.dataset.id = categoriesFiltres.id;
+      const filtresTitre = document.createElement("p");
+      filtresTitre.innerText = categoriesFiltres.name;
+      filtresProjet.appendChild(filtresNav);
+      filtresNav.appendChild(filtres);
+      filtres.appendChild(filtresTitre);
+    }
+}
+
+//Fonction pour Filtrer les projets
+function filtrerProjets(){
+  const btnTri = document.querySelectorAll("div[data-id]");
+
+  btnTri.forEach((btn) => {
+    btn.addEventListener("click", function(){
+      const idProjet = btn.getAttribute("data-id");
+
+      let projetFiltre;
+      if (idProjet === "0") {
+        projetFiltre = projets
+      }
+      else{
+      projetFiltre = projets.filter((projet) => projet.category.id === parseInt(idProjet));
+    }
+      projetsGallery(projetFiltre);
+    })
+  });
+}
+
+//Lancement des fonctions
+projetsGallery(projets);
+
+filtresCategories(categories);
+
+filtrerProjets();
